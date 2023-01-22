026d94c06877434494515fe81bd52869da4d7575
221075 <221075@ssg.com>
2023-01-22 23:44:13 +0900

    feat : StepExecution 실습 (실패 Step 재처리 학습)

---
+++ src/main/java/io/springbatch/springbatch/stepexecution/StepExecutionConfiguration.java
@@ -0,0 +1,66 @@
+package io.springbatch.springbatch.stepexecution;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * StepExecution
+ */
+/*
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class StepExecutionConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    log.info("[StepConfiguration] - step1 execute");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((stepContribution, chunkContext) -> {
+                    log.info("[StepConfiguration] - step2 execute");
+                    // throw new RuntimeException("[StepConfiguration] - step2 fail");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((stepContribution, chunkContext) -> {
+                    log.info("[StepConfiguration] - step3 execute");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
+*/
