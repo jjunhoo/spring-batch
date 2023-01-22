e722879a9bac1b702ee182ae23bacf46cc98568f
221075 <221075@ssg.com>
2023-01-22 22:45:05 +0900

    feat : JobExecution 실습 (성공/실패 여부 확인)

---
+++ src/main/java/io/springbatch/springbatch/JobExecutionConfiguration.java
@@ -0,0 +1,63 @@
+package io.springbatch.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * JobExecution
+ */
+/*
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class JobExecutionConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        log.info("[JobExecutionConfiguration] - step1 execute");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((stepContribution, chunkContext) -> {
+                    log.info("[JobExecutionConfiguration] - step2 execute");
+                    // 예외 발생으로 인하여 'BATCH_JOB_EXECUTION' 테이블의 'STATUS' 컬럼이 'FAILED' 로 INSERT 됨
+                    // throw new RuntimeException("[JobExecutionConfiguration] - step2 execute fail"); // java.lang.RuntimeException: [JobExecutionConfiguration] - step2 execute fail
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
+*/
